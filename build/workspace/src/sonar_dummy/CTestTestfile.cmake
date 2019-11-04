6195ca8f6c4e217d4c3738a524cfe1c2a8804a92
Christian Bouchard <christian.bouchard@cidco.ca>
2019-11-04 14:35:15 -0500

    Removed files from build and devel

--- build/workspace/src/sonar_dummy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cidco-hydroball/Workspace/Poseidon/src/workspace/src/sonar_dummy
-# Build directory: /home/cidco-hydroball/Workspace/Poseidon/build/workspace/src/sonar_dummy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

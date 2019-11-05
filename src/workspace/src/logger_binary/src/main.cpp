3b9c5ab87a725e2fbc0860f945b711053c795047
Christian Bouchard <christian.bouchard@cidco.ca>
2019-11-05 12:57:54 -0500

    Changed global variables to local variables in loggers, when possible

--- src/workspace/src/logger_binary/src/main.cpp
+++ src/workspace/src/logger_binary/src/main.cpp
@@ -1,3 +1,5 @@
+// logger_binary
+
 #ifndef MAIN_CPP
 #define MAIN_CPP
 
@@ -7,7 +9,7 @@
 #include "std_msgs/String.h"
 #include <ctime>
 #include <iostream>
-#include <fstream>
+// #include <fstream>
 #include <sstream>
 #include <string>
 #include <stdint.h>
@@ -15,51 +17,27 @@
 #include <inttypes.h>
 #include "../../utils/ReaderWriter.hpp"
 
-Position pos;
-Imu imu;
-Sonar sonar;
+// Position pos;
+// Imu imu;
+// Sonar sonar;
+
+
+// It would be possible to have the callback functions
+// be part of class Writer so that a global variable Writer *writer
+// would not be required
+// http://docs.ros.org/melodic/api/roscpp/html/classros_1_1NodeHandle.html#a317fe4c05919e0bf3fb5162ccb2f7c28
 
 Writer *writer;
 
-std::string outputGnssFile;
-std::string outputImuFile;
-std::string outputSonarFile;
-std::string outputFolder;
-std::ofstream outputGnss;
-std::ofstream outputImu;
-std::ofstream outputSonar;
-std::string sep = ";";
+// std::string outputGnssFile;
+// std::string outputImuFile;
+// std::string outputSonarFile;
+// std::string outputFolder;
 
-uint64_t buildTimeStamp(int sec, int nsec);
-
-void gnssCallback(const geometry_msgs::PoseStamped& gnss)
-{
-  pos.timeStamp = buildTimeStamp(gnss.header.stamp.sec, gnss.header.stamp.nsec);
-  pos.x = gnss.pose.position.x;
-  pos.y = gnss.pose.position.y;
-  pos.z = gnss.pose.position.z;
-
-  writer->writeBinGnss(pos);
-}
-
-void imuCallback(const geometry_msgs::PoseStamped& imuMsgs)
-{
-  imu.timeStamp = buildTimeStamp(imuMsgs.header.stamp.sec, imuMsgs.header.stamp.nsec);
-  imu.w = imuMsgs.pose.orientation.w;
-  imu.x = imuMsgs.pose.orientation.x;
-  imu.y = imuMsgs.pose.orientation.y;
-  imu.z = imuMsgs.pose.orientation.z;
-
-  writer->writeBinImu(imu);
-}
-
-void sonarCallback(const geometry_msgs::PointStamped& sonarMsgs)
-{
-  sonar.timeStamp = buildTimeStamp(sonarMsgs.header.stamp.sec, sonarMsgs.header.stamp.nsec);
-  sonar.depth = sonarMsgs.point.z;
-
-  writer->writeBinSonar(sonar);
-}
+// std::ofstream outputGnss;
+// std::ofstream outputImu;
+// std::ofstream outputSonar;
+// std::string sep = ";";
 
 uint64_t buildTimeStamp(int sec, int nsec){
   std::ostringstream os;
@@ -72,6 +50,41 @@ uint64_t buildTimeStamp(int sec, int nsec){
   iss >> timestamp;
 
   return timestamp;
+}
+
+void gnssCallback(const geometry_msgs::PoseStamped& gnss)
+{
+  Position pos;
+
+  pos.timeStamp = buildTimeStamp(gnss.header.stamp.sec, gnss.header.stamp.nsec);
+  pos.x = gnss.pose.position.x;
+  pos.y = gnss.pose.position.y;
+  pos.z = gnss.pose.position.z;
+
+  writer->writeBinGnss(pos);
+}
+
+void imuCallback(const geometry_msgs::PoseStamped& imuMsgs)
+{
+  Imu imu;
+
+  imu.timeStamp = buildTimeStamp(imuMsgs.header.stamp.sec, imuMsgs.header.stamp.nsec);
+  imu.w = imuMsgs.pose.orientation.w;
+  imu.x = imuMsgs.pose.orientation.x;
+  imu.y = imuMsgs.pose.orientation.y;
+  imu.z = imuMsgs.pose.orientation.z;
+
+  writer->writeBinImu(imu);
+}
+
+void sonarCallback(const geometry_msgs::PointStamped& sonarMsgs)
+{
+  Sonar sonar;
+  sonar.timeStamp = buildTimeStamp(sonarMsgs.header.stamp.sec,
+                                    sonarMsgs.header.stamp.nsec);
+  sonar.depth = sonarMsgs.point.z;
+
+  writer->writeBinSonar(sonar);
 }
 
 int main(int argc, char **argv)
@@ -92,14 +105,24 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  std::string folderPath(argv[1]);
-  outputFolder = folderPath;
+  // std::string folderPath(argv[1]);
+  // std::string outputFolder = folderPath;
 
-  outputGnssFile = outputFolder + "/" + getStringDate() + "_gnss.bin";
-  outputImuFile = outputFolder + + "/" + getStringDate() + "_imu.bin";
-  outputSonarFile = outputFolder + "/" + getStringDate() + "_sonar.bin";
+  std::string outputFolder(argv[1]);
+
+  std::string outputGnssFile = outputFolder + "/" 
+                                    + getStringDate() + "_gnss.bin";
+  std::string outputImuFile = outputFolder + + "/" 
+                                    + getStringDate() + "_imu.bin";
+  std::string outputSonarFile = outputFolder + "/" 
+                                    + getStringDate() + "_sonar.bin";
 
   writer = new Writer(outputGnssFile, outputImuFile, outputSonarFile, true);
+
+  if ( writer->getSetupOK() == false ) {
+    std::cout << "logger_binary, could not setup the writer" << std::endl;
+    return 1;
+  }
 
   ros::init(argc, argv, "logger_binary");
 

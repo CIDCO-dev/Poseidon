62d66e227c1f966a66669e0e6c14f0d68fe22879
Christian Bouchard <christian.bouchard@cidco.ca>
2019-11-05 11:34:54 -0500

    Bash script to launch 'roslaunch'

---
+++ launchWithRoslaunch.sh
@@ -0,0 +1,6 @@
+source devel/setup.bash
+
+roslaunch src/workspace/launch/hydroball.launch loggerPath:="$1"
+
+#roslaunch src/workspace/launch/hydroball.launch loggerPath:="/home/cidco-hydroball/Documents/PoseidonOutput"
+

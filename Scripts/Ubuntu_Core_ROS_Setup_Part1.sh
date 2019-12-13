a8afa09ed1aa83da38dabc311d4574e3b4a1a229
Ddoiron-cidco <58708562+Ddoiron-cidco@users.noreply.github.com>
2019-12-13 16:20:47 -0500

    Update Ubuntu_Core_ROS_Setup_Part1.sh

--- Scripts/Ubuntu_Core_ROS_Setup_Part1.sh
+++ Scripts/Ubuntu_Core_ROS_Setup_Part1.sh
@@ -8,7 +8,7 @@
 snap refresh
 
 # Install the classic shell (To use apt)
-snap install classic --/beta --devmode
+snap install classic --beta --devmode
 echo "######################################"
 echo "# Part 1 DONE, now onto Part 2       #"
 echo "# When the running command is over,  #"

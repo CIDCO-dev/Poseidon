80fcf05990b02759cec7b1f94886f7a245bb5cc0
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2019-12-18 10:08:45 -0500

    Update wifi-ap.sh
    ne peut etre configurer par netplan
    
    dois etre fait avec network-manager ce qui implante le serveur dhcp automatiquement. le range d'ip est 10.42.0.***
    
    Le tout vas etre implémenter dans le scipt de conffiguration ubuntu server

--- Scripts/wifi-ap.sh
+++ Scripts/wifi-ap.sh
@@ -1,12 +1,4 @@
-sudo echo    wifis: >> /etc/netplan/0-cloud-init.yaml
-sudo echo        wlan0: >> /etc/netplan/0-cloud-init.yaml
-sudo echo            optional: true >> /etc/netplan/0-cloud-init.yaml
-sudo echo            access-points: >> /etc/netplan/0-cloud-init.yaml
-sudo echo                "Hydro-B": >> /etc/netplan/0-cloud-init.yaml
-sudo echo                    password: "test" >> /etc/netplan/0-cloud-init.yaml
-sudo echo            dhcp4: no >> /etc/netplan/0-cloud-init.yaml
-sudo echo            dhcp6: no >> /etc/netplan/0-cloud-init.yaml
-sudo echo            addresses: [192.168.0.1/24] >> /etc/netplan/0-cloud-init.yaml
-sudo echo            gateway4: 192.168.0.1 >> /etc/netplan/0-cloud-init.yaml
-
-
+sudo apt-get install network-manager
+sudo systemctl start NetworkManager.service
+sudo systemctl enable NetworkManager.service
+nmcli dev wifi hotspot ifname wlan0 ssid Hydro-B password "cidco1234"

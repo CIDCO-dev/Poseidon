c1aa9fc597a871bccebca9e6500a61b982934396
Dany <dany.doiron@cidco.ca>
2020-03-04 17:02:34 -0500

    enregistrement d'un ficiher avec le caractere 1 en fonction de la date et de l'heure de lordinateur il existe un petit probleme pour transferer le path dans le process

--- src/workspace/src/gnss_zed-f9p/src/main.cpp
+++ src/workspace/src/gnss_zed-f9p/src/main.cpp
@@ -3,9 +3,16 @@
 
 #include "ros/ros.h"
 
+#include <fstream>
 #include <iostream>
+#include <chrono>
+#include <ctime> 
+#include <string>
 
+using namespace std;
 
+string fnameout="123";
+string path="123";
 
 class MOTOR{
 	private:
@@ -14,23 +21,57 @@ class MOTOR{
 
 	MOTOR(){
 		}
+	std::string datetime(){
+    		time_t rawtime;
+    		struct tm * timeinfo;
+    		char buffer[80];
 
+    		time (&rawtime);
+    		timeinfo = localtime(&rawtime);
+
+    		strftime(buffer,80,"%Y%m%d%H%M%S",timeinfo);
+    		return std::string(buffer);
+}
 
 
 	void run(){
 		while(ros::ok()){
 		//lis le temps si la seconde change on change le nom du fichier
+		path = "/home/ubuntu/ubx/";
+
+		fnameout = path + datetime() +".ubx";
 		//resoit les donnée du port série /dev/ttyACM0
+		
+
 		//ouvre le fichier
-		//écrit les donée recu du port série dans le fichier	
-		//ferme le fichier	
+		ofstream file;
+		file.open(fnameout); // File output stream, the file that we'll write to it
+		if(!file) { // if can't open file
+		}
+		//écrit les donée recu du port série dans le fichier
+		file << 1 << endl;
+		//ferme le fichier
+		file.close();
+	
 		}
 	}
 
 
 };
 int main(int argc,char** argv){
+  	if(argc < 2){
+    		std::cout << "nlogger_text, Missing output folder path" << std::endl;
+    		return 1;
+  	}
+	std::string outputFolder( argv[1] );
+	
+	outputFolder = "/home/ubuntu/ubx/";
+
 	ros::init(argc, argv, "motor");
+
+	//récupération du path
+	//création du path du log si non présent
+	;
 	//initialiser le module zed-f9p
 
 	MOTOR motor;

efa8cc41cf06bfaf70945a43878b15c1b2e144ff
dany <dany.doiron@cidco.ca>
2020-04-02 09:19:13 -0400

    correction du module pour l'ins inertial sense et ajout du module de test catch-ros et premier test pour dummy gnss

---
+++ src/workspace/src/inertial_sense_ros/include/InertialSenseSDK/src/ring_buffer.h
@@ -0,0 +1,49 @@
+/*
+ *  ring_buffer.h
+ *
+ *  Created on: May 5, 2011
+ *      Author: waltj
+ */
+
+#ifndef _RING_BUFFER_H_
+#define _RING_BUFFER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+//_____ D E F I N I T I O N S ______________________________________________
+
+typedef struct
+{
+	unsigned char *startPtr;        // Pointer to buffer start (lowest address)
+	unsigned char *endPtr;          // Pointer to one past buffer end (buffer start + sizeof(buf)).
+	unsigned char *rdPtr;           // Buffer read pointer
+	unsigned char *wrPtr;           // Buffer write pointer
+	int bufSize;                    // Byte size of buffer
+} ring_buf_t;
+
+
+//_____ P R O T O T Y P E S ________________________________________________
+
+void ringBufInit(ring_buf_t *rbuf, unsigned char* buf, int bufSize);
+int ringBufUsed(ring_buf_t *rbuf);
+int ringBufFree(ring_buf_t *rbuf);
+int ringBufWrite(ring_buf_t *rbuf, unsigned char *buf, int numBytes);
+int ringBufRead(ring_buf_t *rbuf, unsigned char *buf, int len);
+int ringBufPeek(ring_buf_t *rbuf, unsigned char *buf, int len, int offset);
+int ringBufReadToChar(ring_buf_t *rbuf, unsigned char *buf, int len, unsigned char character);
+int ringBufReadToChar2(ring_buf_t *rbuf, unsigned char *buf, int len, unsigned char character1, unsigned char character2);
+int ringBufFind(ring_buf_t *rbuf, const unsigned char *str, int len);
+int ringBufRemove(ring_buf_t *rbuf, int len);
+int ringBufClear(ring_buf_t *rbuf);
+int ringBufEmpty(ring_buf_t *rbuf);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // _RING_BUFFER_H_

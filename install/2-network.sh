38ad34aed90accffb7b92ec23ca5131f7554b9ae
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2020-05-21 13:09:00 -0400

    Update 2-network.sh
    correction pour la gestion du nouveau site web

--- install/2-network.sh
+++ install/2-network.sh
@@ -42,11 +42,34 @@ sudo systemctl enable lighttpd.service | tee -a log.txt
 echo "[+] Installing websocket library"
 sudo apt install libwebsocketpp-dev
 
-echo "[+] Installing web UI"
-sudo mkdir -p /var/www
-sudo chmod +644 /var/www
-cd /var/www/
-git clone https://github.com/Ddoiron-cidco/Poseidon_web.git
-sudo rm -r -d html  | tee -a log.txt
-mv Poseidon_web html
+sudo bash -c 'cat << EOF2 > /etc/lighttpd/lighttpd.conf
+server.modules = (
+	"mod_access",
+	"mod_alias",
+	"mod_compress",
+ 	"mod_redirect",
+)
 
+server.document-root        = "/home/ubuntu/Poseidon/www/webroot"
+server.upload-dirs          = ( "/var/cache/lighttpd/uploads" )
+server.errorlog             = "/var/log/lighttpd/error.log"
+server.pid-file             = "/var/run/lighttpd.pid"
+server.username             = "www-data"
+server.groupname            = "www-data"
+server.port                 = 80
+
+
+index-file.names            = ( "index.php", "index.html", "index.lighttpd.html" )
+url.access-deny             = ( "~", ".inc" )
+static-file.exclude-extensions = ( ".php", ".pl", ".fcgi" )
+
+compress.cache-dir          = "/var/cache/lighttpd/compress/"
+compress.filetype           = ( "application/javascript", "text/css", "text/html", "text/plain" )
+
+# default listening port for IPv6 falls back to the IPv4 port
+## Use ipv6 if available
+#include_shell "/usr/share/lighttpd/use-ipv6.pl " + server.port
+include_shell "/usr/share/lighttpd/create-mime.assign.pl"
+include_shell "/usr/share/lighttpd/include-conf-enabled.pl"
+EOF2'
+sudo systemctl reload lighttpd.service

7ba93ab4382c52c47b097e6c4be5ae3cbc119e8b
Poseidon catarob <dany.doiron@cidco.ca>
2020-06-30 15:52:06 +0000

    modification des fichier de configuration pour le script d<installation pour suporter lancien ordinateur de bord du catarob

--- install/3-network.sh
+++ install/3-network.sh
@@ -1,39 +1,5 @@
 #!/bin/sh
 
-echo "Configuring network"
-# install network-manager and install as service
-sudo apt-get install network-manager -y | tee -a log.txt
-sudo systemctl start NetworkManager.service | tee -a log.txt
-sudo systemctl enable NetworkManager.service | tee -a log.txt
-
-echo "Creating WiFi hotspot"
-sudo nmcli dev wifi hotspot ifname wlan0 ssid Hydro-B password "cidco1234" | tee -a log.txt
-sudo nmcli con modify Hotspot autoconnect yes
-sudo nmcli con modify Hotspot ipv4.addresses 192.168.1.1/24,192.168.1.1
-sudo nmcli con reload
-sudo service network-manager restart
-
-sudo bash -c 'cat << EOF2 > /etc/netplan/50-cloud-init.yaml
-# This file is generated from information provided by
-# the datasource.  Changes to it will not persist across an instance.
-# To disable cloud-init s network configuration capabilities, write a file
-# /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg with the following:
-# network: {config: disabled}
-network:
-    version: 2
-    ethernets:
-        eth0:
-            dhcp4: true
-            optional: true
-            match:
-                name: eth0
-            set-name: eth0
-            addresses:
-              - 192.168.2.101/24
-EOF2'
-
-sudo netplan apply
-
 echo "[+] Installing web server"
 sudo apt install lighttpd -y | tee -a log.txt
 

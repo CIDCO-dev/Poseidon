d9f772c9ac8db6ad87dec185270ab2097a77e16e
ubuntu catarob <dany.doiron@cidco.ca>
2020-07-27 13:08:34 +0000

    correction script 6-catarob pour supporter les log files

--- install/6-catarob.sh
+++ install/6-catarob.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-sudo apt install -y libeigen3-dev libgeographic-dev ros-melodic-robot-localization
+sudo apt install -y libeigen3-dev libgeographic-dev ros-melodic-robot-localization  | tee log.txt
 
-cd /home/ubuntu/
-git clone https://github.com/orocos/orocos_kinematics_dynamics.git
-cd orocos_kinematics_dynamics
-cd orocos_kdl
-cmake .
-make
-sudo make install
+cd /home/ubuntu/  | tee log.txt
+git clone https://github.com/orocos/orocos_kinematics_dynamics.git  | tee log.txt
+cd orocos_kinematics_dynamics  
+cd orocos_kdl  
+cmake .  | tee log.txt
+make  | tee log.txt
+sudo make install  | tee log.txt
 cd ..
 cd ..
 
-git clone https://github.com/jbeder/yaml-cpp.git
+git clone https://github.com/jbeder/yaml-cpp.git  | tee log.txt
 cd yaml-cpp
-cmake -DYAML_BUILD_SHARED_LIBS=ON .
-make
-sudo make install
+cmake -DYAML_BUILD_SHARED_LIBS=ON .  | tee log.txt
+make  | tee log.txt
+sudo make install  | tee log.txt
 cd ..

ac4e60561a479526fab15f09e11d3c13ac30df44
ddoiron-cidco vm <dany.doiron@cidco.ca>
2020-10-06 19:41:25 +0000

    simplify install script for user

---
+++ install/x64.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+~/Poseidon/install/Script/1-base-ros.sh
+
+~/Poseidon/install/Script/2-x64.sh
+
+~/Poseidon/install/Script/3-network.sh
+
+~/Poseidon/install/Script/4-x64.sh
+
+~/Poseidon/install/Script/5-finalize.sh
+

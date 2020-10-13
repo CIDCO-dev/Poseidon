3f0813cf317c439a425f19b5cb09556e097f02c0
Dany <dany.doiron@cidco.ca>
2020-10-13 10:00:34 -0400

    fixing chrony and moving one install/script

--- install/Script/5-finalize.sh
+++ install/Script/5-finalize.sh
@@ -12,13 +12,13 @@ sudo apt-get install chrony -y | tee -a log.txt
 
 sudo bash -c 'cat << EOF2 > /etc/chrony/chrony.conf
 # PPS: /dev/pps0: Kernel-mode PPS ref-clock for the precise seconds
-refclock  PPS /dev/pps0  refid PPS  precision 1e-9  poll 3  trust  prefer
+refclock  PPS /dev/pps0  refid PPS  precision 1e-9  poll 3  trust 
 # SHM(2), gpsd: PPS data from shared memory provided by gpsd
 #refclock  SHM 2  refid PPSx  precision 1e-9  poll 3  trust
 # SOCK, gpsd: PPS data from socket provided by gpsd
 #refclock  SOCK /var/run/chrony.pps0.sock  refid PPSy  precision 1e-9  poll 3  trust
 # SHM(0), gpsd: NMEA data from shared memory provided by gpsd
-refclock  SHM 0  refid NMEA  precision 1e-3  offset 0.5  delay 0.2  poll 3  trust  require
+refclock  SHM 0  refid NMEA  precision 1e-3  offset 0.5  delay 0.2  poll 3  trust  prefer
 # any NTP clients are allowed to access the NTP server.
 allow
 # allows to appear synchronised to NTP clients, even when it is not.

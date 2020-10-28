52fa397854d5d9dd456e604ee951745c415db878
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2020-10-28 10:59:42 -0400

    Update launchHydrobox.sh

--- launchHydrobox.sh
+++ launchHydrobox.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ ! -d "~/Poseidon/www/webroot/record" ] && mkdir -p "`/Poseidon/www/webroot/record"
+
 source /opt/ros/melodic/setup.bash
 source /home/ubuntu/Poseidon/src/workspace/devel/setup.bash
 

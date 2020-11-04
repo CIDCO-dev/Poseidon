9d5814f19a2ec9fdc3dfa2f07b6758ca27bb69bc
ddoiron rpi hydrob <dany.doiron@cidco.ca>
2020-11-04 15:24:05 +0000

    remove the bad fix on launchHydrobox.sh

--- launchHydrobox.sh
+++ launchHydrobox.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd /home/ubuntu/Poseidon/www/webroot
-[ ! -d "/record" ] && mkdir record
-cd /home/ubuntu/Poseidon
-
 
 source /opt/ros/melodic/setup.bash
 source /home/ubuntu/Poseidon/src/workspace/devel/setup.bash

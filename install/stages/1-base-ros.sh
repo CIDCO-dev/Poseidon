1f51c73a3aa3923cb99663469887e928084bd9ce
ddoiron rpi hydrob <dany.doiron@cidco.ca>
2020-11-05 16:12:25 +0000

    update the install files to add apt install zip

--- install/stages/1-base-ros.sh
+++ install/stages/1-base-ros.sh
@@ -10,7 +10,7 @@ echo "[+] Updating applications"
 sudo apt upgrade -y | tee -a log.txt
 
 echo "[+] Installing toolchain"
-sudo apt install gcc python3-dev python3-pip python3-setuptools git curl -y | tee -a log.txt
+sudo apt install gcc python3-dev python3-pip python3-setuptools git curl zip -y | tee -a log.txt
 
 echo "[+] Installing ROS"
 

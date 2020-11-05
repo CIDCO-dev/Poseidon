0ec4affd08703878e952b0659acdde2dee831aea
ddoiron rpi hydrob <dany.doiron@cidco.ca>
2020-11-05 16:07:59 +0000

    fix the rosbag zip creation to remove directory in the zip file

--- rec_rosbag.sh
+++ rec_rosbag.sh
@@ -5,7 +5,7 @@ source src/workspace/devel/setup.bash
 
 roslaunch src/workspace/launch/rec_rosbag.launch time_now:=$(date +%Y.%m.%d_%H%M%S)
 
-zip -9 -rm /home/ubuntu/$(date +%Y.%m.%d_%H%M%S)_rosbag.zip /home/ubuntu/*.bag
+zip -9 -rmj /home/ubuntu/$(date +%Y.%m.%d_%H%M%S)_rosbag.zip /home/ubuntu/*.bag
 
 sudo mv /home/ubuntu/*.zip /home/ubuntu/Poseidon/www/webroot/record/
 

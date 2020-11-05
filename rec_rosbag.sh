d22ce05ae40bb7036cf1a87195a75bf5ce955c92
ddoiron rpi hydrob <dany.doiron@cidco.ca>
2020-11-05 15:55:50 +0000

    fix the rosbag file name with the date. compress the bag files in a zip and move it in the record directory. pute the date in the zip file name.

--- rec_rosbag.sh
+++ rec_rosbag.sh
@@ -3,4 +3,9 @@
 source /opt/ros/melodic/setup.bash
 source src/workspace/devel/setup.bash
 
-roslaunch src/workspace/launch/rec_rosbag.launch
+roslaunch src/workspace/launch/rec_rosbag.launch time_now:=$(date +%Y.%m.%d_%H%M%S)
+
+zip -9 -rm /home/ubuntu/$(date +%Y.%m.%d_%H%M%S)_rosbag.zip /home/ubuntu/*.bag
+
+sudo mv /home/ubuntu/*.zip /home/ubuntu/Poseidon/www/webroot/record/
+

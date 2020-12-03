477414b4c0e12f2b3b34a827a4f008f94f0ee64d
Jordan McManus <jordan.mcmanus@cidco.ca>
2020-12-03 09:06:16 -0500

    moved define above includes

--- src/workspace/src/utils/QuaternionUtils.h
+++ src/workspace/src/utils/QuaternionUtils.h
@@ -1,11 +1,10 @@
 #ifndef QUATERNIONUTILS_H
 #define QUATERNIONUTILS_H
 
+#define TF2_EULER_DEFAULT_ZYX 1
+
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
-
-#define TF2_EULER_DEFAULT_ZYX 1
 
 #include "Constants.hpp"
 

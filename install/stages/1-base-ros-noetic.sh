0b67e4a3ae84f9358a9c74d53646e4f280bd834b
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2021-06-23 14:16:00 -0400

    Update 1-base-ros-noetic.sh
    replace the source key for noetic

--- install/stages/1-base-ros-noetic.sh
+++ install/stages/1-base-ros-noetic.sh
@@ -18,8 +18,7 @@ echo "[+] Installing ROS"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' | tee -a log.txt
 
 # Setup keys
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 | tee -a log.txt
-curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add - | tee -a log.txt
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 # Refresh repos with new ROS repos
 sudo apt-get update | tee -a log.txt

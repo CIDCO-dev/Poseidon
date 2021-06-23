ddb6a9c3c930773120fc4d82769525316bcac7ca
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2021-06-23 14:22:02 -0400

    Create x64-noetic.sh

---
+++ install/x64-noetic.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+~/Poseidon/install/stages/1-base-ros-noetic.sh
+
+~/Poseidon/install/stages/2-x64.sh
+
+~/Poseidon/install/stages/3-network.sh
+
+~/Poseidon/install/stages/4-x64.sh
+
+~/Poseidon/install/stages/5-finalize.sh

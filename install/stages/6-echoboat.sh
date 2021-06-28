4bce8710384f8f819baacb07d843e1f6889c6072
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2021-06-28 16:24:43 -0400

    Update 6-echoboat.sh

--- install/stages/6-echoboat.sh
+++ install/stages/6-echoboat.sh
@@ -9,6 +9,8 @@ sudo apt install ros-noetic-mavros ros-noetic-mavros-extras -y
 echo "Download VLP-16 driver"
 sudo apt-get install ros-noetic-velodyne
 git clone https://github.com/ros-drivers/velodyne.git
+
+rosdep update
 rosdep install --from-paths velodyne/ --ignore-src --rosdistro noetic -y
 
 

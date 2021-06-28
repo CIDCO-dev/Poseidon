8513ed1cb2e27589440a48254f3b174c7f1773dd
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2021-06-28 14:28:15 -0400

    Update 6-echoboat.sh

--- install/stages/6-echoboat.sh
+++ install/stages/6-echoboat.sh
@@ -9,9 +9,8 @@ git clone https://github.com/mavlink/mavros.git
 echo "Download VLP-16 driver"
 sudo apt-get install ros-noetic-velodyne
 git clone https://github.com/ros-drivers/velodyne.git
-rosdep install --from-paths src --ignore-src --rosdistro noetic -y
+rosdep install --from-paths velodyne/ --ignore-src --rosdistro noetic -y
 
-git clone https://github.com/ros-perception/perception_pcl.git
 
 echo "Download sbg driver"
 git clone https://github.com/SBG-Systems/sbg_ros_driver.git

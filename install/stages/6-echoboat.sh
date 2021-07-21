0c46a551c27a155fcc8ad4a2976272e06dc66e8d
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2021-07-21 13:39:53 -0400

    Update 6-echoboat.sh

--- install/stages/6-echoboat.sh
+++ install/stages/6-echoboat.sh
@@ -17,3 +17,6 @@ rosdep install --from-paths velodyne/ --ignore-src --rosdistro noetic -y
 echo "Download sbg driver"
 git clone https://github.com/SBG-Systems/sbg_ros_driver.git
 sudo adduser $USER dialout
+
+cd /home/ubuntu/Poseidon/sec/workspace
+catkin_make -j1

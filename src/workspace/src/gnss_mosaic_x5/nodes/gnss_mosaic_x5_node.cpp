1f97432c8e0634bb51bfbd73ad50a13902e9cb3e
JordanMcManus <jordan.mcmanus@cidco.ca>
2021-09-29 19:16:56 -0400

    fixed bugs

--- src/workspace/src/gnss_mosaic_x5/nodes/gnss_mosaic_x5_node.cpp
+++ src/workspace/src/gnss_mosaic_x5/nodes/gnss_mosaic_x5_node.cpp
@@ -1,12 +1,12 @@
 #include "ros/ros.h"
-#include "gnss_zed_f9p/gnss_zed_f9p.h"
+#include "gnss_mosaic_x5/gnss_mosaic_x5.h"
 
 
 int main(int argc,char** argv){
-	ros::init(argc, argv, "zedf9p");
+	ros::init(argc, argv, "mosaic-X5");
 
 	if(argc != 3){
-		std::cerr << "gnss_zed_f9p logPath serialDevice" << std::endl;
+		std::cerr << "gnss_mosaic_x5 logPath serialDevice" << std::endl;
 		exit(1);
 	}
 
@@ -17,14 +17,14 @@ int main(int argc,char** argv){
 		ROS_INFO("Missing output log path\n");
     		return 1;
 	}
-	ROS_INFO("Writing UBX log to %s",logPath.c_str());
+	ROS_INFO("Writing sbf log to %s",logPath.c_str());
 	if (serialPortPath.length()<2){
 		ROS_INFO("Missing serial port path\n");
     		return 1;
 	}
 	ROS_INFO("Using serial port at %s",serialPortPath.c_str());
 
-	ZEDF9P zedf9p(logPath , serialPortPath);
-	zedf9p.run();
+	MosaicX5 mosaicX5(logPath , serialPortPath);
+	mosaicX5.run();
 
 }

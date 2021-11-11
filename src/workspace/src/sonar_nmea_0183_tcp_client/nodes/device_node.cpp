ed927664f6e1a416c4087bc8991b54cbefc928a4
Patricm CM <patrick.cmorneau@cidco.ca>
2021-11-11 14:30:54 -0500

    argument fix

--- src/workspace/src/sonar_nmea_0183_tcp_client/nodes/device_node.cpp
+++ src/workspace/src/sonar_nmea_0183_tcp_client/nodes/device_node.cpp
@@ -6,8 +6,13 @@ int main(int argc,char** argv){
 
 	std::string device;
 
-	//if no params present. use default values of 127.0.0.1:5000
-	if(!ros::param::get("device", device)){
+	//if no params present. use default values of /dev/sonar
+	/* in launch file , private parameter in parameter server
+	<node pkg="sonar_nmea_0183_tcp_client" type="nmea_device_node" name="Sonar" output="screen" respawn="true" respawn_delay="1">
+		<param name="device" type="str" value="/dev/ttyUSB0"/>
+	</node>
+	*/
+	if(!ros::param::get("/Sonar/device", device)){
 		device = "/dev/sonar";
 	}
 

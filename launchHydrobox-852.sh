8c64caa27e3cb3490f25775aea7b9ca6bd2172a3
Dany Doiron <dany.doiron@cidco.ca>
2021-12-15 11:56:21 -0500

    fix launch script

--- launchHydrobox-852.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/noetic/setup.bash
-source /home/ubuntu/Poseidon/src/workspace/devel/setup.bash
-
-roslaunch /home/ubuntu/Poseidon/src/workspace/launch/hydrobox_imagenex852.launch time_now:=$(date +%Y.%m.%d_%H%M%S)
-
-
-
--- launchHydrobox.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#/home/ubuntu/Poseidon/launchHydrobox-852-x52.sh
-#/home/ubuntu/Poseidon/launchHydrobox-nmea-x5.sh
-/home/ubuntu/Poseidon/launchHydrobox-852-piksi.sh
-#/home/ubuntu/Poseidon/launchHydrobox-nmea-piksi.sh

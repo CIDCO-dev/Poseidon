66d7657ebf73287f1d3e19600869249a56ef1609
Dany Doiron <dany.doiron@cidco.ca>
2022-01-10 05:12:02 +0000

    Added log rotation to logger_text

--- install/stages/1-base-ros-noetic.sh
+++ install/stages/1-base-ros-noetic.sh
@@ -27,7 +27,7 @@ curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo ap
 sudo apt-get update | tee -a log.txt
 
 #sudo apt install ros-kinetic-ros-base g++ -y >> log.txt
-sudo apt install ros-noetic-ros-base ros-noetic-tf2-geometry-msgs g++ python3-rosdep ros-noetic-mavros -y | tee -a log.txt
+sudo apt install ros-noetic-ros-base ros-noetic-tf2-geometry-msgs g++ python3-rosdep ros-noetic-mavros ros-noetic-sbg-driver -y | tee -a log.txt
 
 echo "[+] Initializing ROS dependencies"
 sudo rosdep init | tee -a log.txt

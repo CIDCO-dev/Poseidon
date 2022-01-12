4ec50e3698ac53283388945b136594649949f59c
Dany Doiron <ddoiron79@gmail.com>
2022-01-12 14:32:27 -0500

    fix launch script name and location. fix ros service script fix other script and file link

--- tools/rec_rosbag.sh
+++ tools/rec_rosbag.sh
@@ -3,7 +3,7 @@
 source /opt/ros/melodic/setup.bash
 source /home/ubuntu/Poseidon/src/workspace/devel/setup.bash
 
-roslaunch /home/ubuntu/Poseidon/src/workspace/launch/rec_rosbag.launch time_now:=$(date +%Y.%m.%d_%H%M%S)
+roslaunch /home/ubuntu/Poseidon/src/workspace/launch/Rosbag/rec_rosbag.launch time_now:=$(date +%Y.%m.%d_%H%M%S)
 
 zip -0 -rmj /home/ubuntu/$(date +%Y.%m.%d_%H%M%S)_rosbag.zip /home/ubuntu/*.bag
 

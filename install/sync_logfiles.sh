279e41e95f9c0b5813f5262bb293572db3048508
patrick <patrick.cmorneau@cidco.ca>
2022-01-20 17:41:43 -0500

    script upgrade

--- install/sync_logfiles.sh
+++ install/sync_logfiles.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-rsync -e'ssh -i ~/.ssh/private_key' -ap '~/Poseidon/www/webroot/record' user@server: destination/folder
+
+DESTINATION_PATH="example/path"
+SERVER="hostname_or_ip"
+USER="Posseidon"
+FILES_LOCATION="~/Poseidon/www/webroot/record"
+PASSWORD_LESS="poseidon"
+
+#add condition to add --remove-source-files to rsync command for testing purposes
+
+rsync -e'ssh -i ~/.ssh/'"$PASSWORD_LESS"'' -apz $FILES_LOCATION $USER@$SERVER:$DESTINATION_PATH

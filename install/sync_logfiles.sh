c5080abc82b081468124dd62a4bfdc63df5db811
patrick <patrick.cmorneau@cidco.ca>
2022-01-21 09:27:26 -0500

    rsync script variables

--- install/sync_logfiles.sh
+++ install/sync_logfiles.sh
@@ -5,7 +5,12 @@ SERVER="hostname_or_ip"
 USER="Posseidon"
 FILES_LOCATION="~/Poseidon/www/webroot/record"
 PASSWORD_LESS="poseidon"
+KEEP_ORIGINAL_FILES=true
 
-#add condition to add --remove-source-files to rsync command for testing purposes
+if $KEEP_ORIGINAL_FILES; then
+	REMOVE_SOURCE_FILES=""
+else
+	REMOVE_SOURCE_FILES="--remove-source-files"
+fi
 
-rsync -e'ssh -i ~/.ssh/'"$PASSWORD_LESS"'' -apz $FILES_LOCATION $USER@$SERVER:$DESTINATION_PATH
+rsync -e'ssh -i ~/.ssh/'"$PASSWORD_LESS"'' -apz $FILES_LOCATION $USER@$SERVER:$DESTINATION_PATH $REMOVE_SOURCE_FILES

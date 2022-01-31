c210085ed03cd5812da229c10e4e82548e0c8047
patrick <patrick.cmorneau@cidco.ca>
2022-01-31 11:25:34 -0500

    class in seperated file

---
+++ src/workspace/src/sonar_nmea_0183_tcp_client/tests/virtual_serial_port.hpp
@@ -0,0 +1,54 @@
+#ifndef virtual_serial_port
+#define virtual_serial_port
+
+
+class virtualSerialPort{
+	public:
+		virtualSerialPort(std::string Slave, std::string Master){
+			this->slave = Slave;
+			this->master = Master;
+		}
+		~virtualSerialPort(){}
+		
+		
+		boost::process::child init(){
+		/*
+		socat should be installed -> sudo apt install socat
+		user should be in dialout group
+		nmea_device_node listen on /dev/sonar
+		$sudo ln -s /home/ubuntu/sonar /dev/sonar
+		$sudo chown -h :dialout /dev/sonar
+		once socat command launched : cat /home/ubuntu/sonar
+		echo test > /home/ubuntu/pty
+		*/
+			//simpliest command
+			//socat -d -d pty,raw,echo=0 pty,raw,echo=0
+			std::string cmd = "socat -d -d pty,raw,echo=0,link="+ this->slave + " pty,raw,echo=0,link=" + this->master;
+			boost::process::child virtDev(cmd); //TODO , handle errors
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			return virtDev;
+		}
+		
+		void write(std::string message){
+			//TODO , handle errors
+			std::ofstream outfile (this->master, std::ofstream::binary);
+			if(outfile.is_open()){
+				outfile << message << "\r\n";
+			}
+			else{
+				std::cerr<<"cannot open master";
+			}
+		}
+		
+		void close(boost::process::child &virtDev){
+			virtDev.terminate();
+			//delete master and slave file ?
+		}
+		
+		
+	private:
+		std::string slave;
+		std::string master;
+};
+
+#endif

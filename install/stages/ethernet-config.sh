adcde4a88289fccca05037827f78d0268066797b
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2022-02-22 13:53:24 -0500

    ethernet-config script ready to deploy

--- install/stages/ethernet-config.sh
+++ install/stages/ethernet-config.sh
@@ -1,7 +1,53 @@
+#!/bin/bash
+
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Ethernet and Wifi Configuration script."
+   echo
+   echo "Syntax: ethernet-config.sh [options]"
+   echo "options:"
+   echo "help or h      Print this Help.                  "    
+   echo "[hotspot_if]     Hotspot interface name.           $hs_if"
+   echo "[hotspot_ssid]   Hotspot SSID.                     $hs_ssid"
+   echo "[hotspot_pass]   Hotspot Password.                 $hs_pass"
+   echo "[wifi_if]        Wifi interface name.              $wf_if"
+   echo "[wifi_ssid]      Wifi SSID.                        $wf_ssid"
+   echo "[wifi_pass]      Wifi Password.                    $wf_pass"
+   echo "[eth_2nd_ip]     Wired ethernet second ip address. $snd_ip"
+   echo
+   echo "Command line exemple."
+   echo "ethernet-config.sh -help"
+   echo "ethernet-config.sh hotspot_if hotspot_ssid -hotspot_pass -wifi_if -wifi_ssid -wifi_pass -eth_2nd_ip "
+   echo "ethernet-config.sh 'wlan1' 'Hydro-B' 'cidco1234' 'wlan0' 'test' 'pass-test' '192.168.2.101'"
+   
+}
+
+############################################################
+# Cfg ethernet                                             #
+############################################################
+Config()
+{
+echo "Hotspot interface name.           $hs_if"
+echo "Hotspot SSID.                     $hs_ssid"
+echo "Hotspot Password.                 $hs_pass"
+echo "Wifi interface name.              $wf_if"
+echo "Wifi SSID.                        $wf_ssid"
+echo "Wifi Password.                    $wf_pass"
+echo "Wired ethernet second ip address. $snd_ip"
+
 echo "Creating WiFi hotspot"
-sudo nmcli dev wifi hotspot ifname wlan1 ssid Hydro-B password "cidco1234" | tee -a log.txt
+sudo nmcli dev wifi hotspot ifname $hs_if ssid $hs_ssid password $hs_pass | tee -a log.txt
 sudo nmcli con modify Hotspot autoconnect yes
 sudo nmcli con modify Hotspot ipv4.addresses 192.168.100.1/24,192.168.100.1
+
+echo "Creating Wifi connection"
+sudo nmcli dev wifi connect $wf_ssid password $wf_pass ifname $wf_if
+
+echo "Apply wifi configuration"
 sudo nmcli con reload
 sudo service network-manager restart
 
@@ -21,14 +67,38 @@ network:
                 name: eth0
             set-name: eth0
             addresses:
-              - 192.168.2.101/24
-    wifis:
-        wlan0:
-            dhcp4: true
-            access-points:
-                "Phyl0":
-                    password: "4012401240124012"
-
+              - '$snd_ip'/24
 EOF2'
 
 sudo netplan apply
+   
+}
+
+
+if [ -z "$1" ] || [ "$1" = 'h' ] || [ "$1" = '-h' ] || [ "$1" = 'help' ] || [ "$1" = '-help' ]
+then
+  Help
+  exit
+else 
+  hs_if=$1
+  hs_ssid=$2
+  hs_pass=$3
+  wf_if=$4
+  wf_ssid=$5
+  wf_pass=$6
+  snd_ip=$7
+  if [ ! -z "$hs_if" ] && [ ! -z "$hs_ssid" ] && [ ! -z "$hs_pass" ] && [ ! -z "$wf_if" ] && [ ! -z "$wf_ssid" ] && [ ! -z "$wf_pass" ] && [ ! -z "$snd_ip" ] 
+  then
+    Config
+    exit
+  else 
+    Help
+    exit
+  fi
+fi
+
+
+
+
+
+

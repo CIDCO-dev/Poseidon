8edfede41ed51fc33148e80200577a8c837284f3
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2022-02-22 10:33:46 -0500

    Update 4-rpi.sh

--- install/stages/4-rpi.sh
+++ install/stages/4-rpi.sh
@@ -7,7 +7,7 @@ cd WiringPi/
 ./build | tee -a log.txt
 
 echo "[+] Configuring UART"
-sudo bash -c 'echo "dtoverlay=disable-bt" >> /boot/firmware/config.txt' | tee -a log.txt
+sudo bash -c 'echo "dtoverlay=disable-bt" >> /boot/firmware/usercfg.txt' | tee -a log.txt
 sudo systemctl mask serial-getty@ttyAMA0.service | tee -a log.txt
 usrmod -G dialout ubuntu
 
@@ -34,15 +34,15 @@ sudo ln -s /lib/systemd/system/gpsd.service /etc/systemd/system/multi-user.targe
 echo "[+] Install PPS"
 sudo apt-get install pps-tools -y | tee -a log.txt
 
-sudo bash -c 'echo "dtoverlay=pps-gpio,gpiopin=4" >> /boot/firmware/config.txt'  
-sudo bash -c 'echo "dtoverlay=uart0" >> /boot/firmware/config.txt'
+sudo bash -c 'echo "dtoverlay=pps-gpio,gpiopin=4" >> /boot/firmware/usercfg.txt'  
+sudo bash -c 'echo "dtoverlay=uart0" >> /boot/firmware/usercfg.txt'
 sudo bash -c 'echo "pps-ldisc" >> /etc/modules'
 
 echo "[+] Config I2C speed"
-sudo bash -c 'echo "dtparam=i2c_arm_baudrate=500000" >> /boot/firmware/config.txt'
+sudo bash -c 'echo "dtparam=i2c_arm_baudrate=500000" >> /boot/firmware/usercfg.txt'
 
 echo "[+] Config uart"
-sudo bash -c 'cat << EOF2 > /boot/firmware/nobtcmd.txt
+sudo bash -c 'cat << EOF2 > /boot/firmware/cmdline.txt
 net.ifnames=0 dwc_otg.lpm_enable=0 console=tty1 root=LABEL=writable rootfstype=ext4 elevator=deadline rootwait fixrtc
 EOF2'
 

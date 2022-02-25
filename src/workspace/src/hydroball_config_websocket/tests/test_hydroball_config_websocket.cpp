d954a45a566c67b8bc5c2c362827f0f8768b4533
patrick <patrick.cmorneau@cidco.ca>
2022-02-25 12:42:03 -0500

    checkpoint config websocket unittest

--- src/workspace/src/hydroball_config_websocket/tests/test_hydroball_config_websocket.cpp
+++ src/workspace/src/hydroball_config_websocket/tests/test_hydroball_config_websocket.cpp
@@ -1,38 +1,59 @@
-
-//#include <gnss_dummy/gnss_dummy.h>
-//#include <ros/ros.h>
 #include <gtest/gtest.h>
+#include <ros/ros.h>
 #include "hydroball_config_websocket/hydroball_config_websocket.h"
-
 #include <thread>
-//#include <chrono>
+
+std::map<std::string, std::string> originalConfig{{"headingOffset", "0"},{"headingOffset", "0"}, {"pitchOffset", "0"},
+												  {"rollOffset", "0"}, {"sonarAbsorbtion", "20"}, {"sonarPulseLength", "120"},
+												  {"sonarRange", "20"}, {"sonarStartGain", "16"}, {"speedThresholdKmh", "6.0"}};
+
+class Config{
+	
+	public:
+		Config(){}
+		~Config(){}
+		void setConfigMap(std::string key, std::string value){
+			configuration[key]=value;
+		}
+		 void getConfigMap(std::map<std::string, std::string> &copy){
+			 copy = this->configuration;
+		}
+	private:
+		std::map<std::string, std::string> configuration;
+};
+
+Config testConfig;
+
+void configurationCallBack(const setting_msg::Setting &setting){
+	ROS_ERROR_STREAM("configCallback -> " << setting.key << " : "<<setting.value<<"\n");
+	testConfig.setConfigMap(setting.key, setting.value);
+}
+
 
 TEST(configWebsocket, configInit) {
 	
-	//ros::NodeHandle n;
-	//ros::Subscriber sub2 = n.subscribe("speed", 10, speedReceived);
-	std::string configFilePath ("/home/ubuntu/Poseidon/config.txt");
+	
+	std::string configFilePath ("/home/ubuntu/Poseidon/src/workspace/src/hydroball_config_websocket/tests/config.txt");
 
 	ConfigurationServer server(configFilePath);
 	uint16_t port = 9004;
 	std::thread t(&ConfigurationServer::run,&server, port);
 	
-	ros::Rate loop_rate( 10 ); // 10 Hz
-	int counter = 0;
-	while(ros::ok()){
-		ros::spinOnce();
-        loop_rate.sleep();
-        counter++;
-        if (counter == 30){
-        	server.stop();
-        	t.join();
-        	ros::shutdown();
-        }
-	}
-		
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("configuration", 1000, configurationCallBack);
+	
+	std::map<std::string, std::string> testConfiguration;
+	testConfig.getConfigMap(testConfiguration);
+	
+	server.broadcastConfiguration();
+	sleep(3);
+	ROS_ERROR_STREAM("nb element: " << testConfiguration.size());
+	//ASSERT_TRUE(testConfiguration.size() == 9);
+	
+	
+	server.stop();
+	t.join();
 	ASSERT_TRUE(true);
-	//ROS_ERROR_STREAM("speed count : "<< speedCounter.getCount()<<"\n");
-	//ASSERT_TRUE(speedCounter.getCount() == 10);
 }
 
 int main(int argc, char** argv) {
@@ -41,12 +62,12 @@ int main(int argc, char** argv) {
     
     testing::InitGoogleTest(&argc, argv);
     
-    //std::thread t([]{while(ros::ok()) ros::spin();});
-    
+    std::thread t([]{ros::spin();}); // let ros spin in its own thread
+     
     auto res = RUN_ALL_TESTS();
     
-    //ros::shutdown();
-    //t.join();
-    
+    ros::shutdown(); // this will cause the ros::spin() to return
+    t.join();
+      
     return res;
 }

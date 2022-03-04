befc9e3490f96af66f3d8a56c07f5f132892eb59
Patricm CM <patrick.cmorneau@cidco.ca>
2022-03-04 11:23:55 -0500

    typo...

--- install/sync_logfiles.sh
+++ install/sync_logfiles.sh
@@ -4,7 +4,7 @@
 DESTINATION_PATH="destination_Path"
 SERVER="hostname or ip"
 USER="HB_XX"
-FILES_LOCATION="/home/ubuntu/Poseidon/www/webroot/record/" # without the lsat '/' it will transfer the folder instead of files
+FILES_LOCATION="/home/ubuntu/Poseidon/www/webroot/record/" # without the last '/' it will transfer the folder instead of files
 PASSWORD_LESS="test"
 KEEP_ORIGINAL_FILES=false # set to true for testing purposes
 

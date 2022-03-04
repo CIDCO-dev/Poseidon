371b441cec7bbf508d492713290a6fff262b4c34
Patricm CM <patrick.cmorneau@cidco.ca>
2022-03-04 11:48:27 -0500

    backing us

--- src/workspace/src/hydroball_config_websocket/tests/test_hydroball_config_websocket.cpp
+++ src/workspace/src/hydroball_config_websocket/tests/test_hydroball_config_websocket.cpp
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2014, Peter Thorson. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the WebSocket++ Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL PETER THORSON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+/*
+modification and test made by: Patrick Charron-Morneau
+date: 04-03-2022
+*/
+
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 #include "hydroball_config_websocket/hydroball_config_websocket.h"
@@ -48,14 +79,13 @@ TEST(configWebsocket, configInit) {
 	
 	//ROS_ERROR_STREAM("nb element: " << testConfiguration.size());
 	ASSERT_TRUE(testConfiguration.size() == 9);
-	bool valid = false;
+	int valid = 0;
 	for(const auto& [key, value]: originalConfig){
 		if(testConfiguration.contains(key) && testConfiguration[key] == value ){
-			valid = true;
+			valid++;
 		}
-		else valid = false;
 	}
-	ASSERT_TRUE(valid);
+	ASSERT_TRUE(valid==9);
 	
 	server.stop();
 	t.join();

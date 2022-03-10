8be3cc7a4ac2a364d22e4beca1892b79745ea0fb
Patricm CM <patrick.cmorneau@cidco.ca>
2022-03-10 08:40:57 -0500

    KEEP_ORIGINAL_FILES set to false

--- install/sync_logfiles.sh
+++ install/sync_logfiles.sh
@@ -5,7 +5,7 @@ DESTINATION_PATH=$2
 SERVER=$3
 RSA_KEY_FILENAME=$4 # path = /home/ubuntu/.ssh/
 FILES_LOCATION="/home/ubuntu/Poseidon/www/webroot/record/" # without the last '/' it will transfer the folder instead of files
-KEEP_ORIGINAL_FILES=true # set to true for testing purposes
+KEEP_ORIGINAL_FILES=false # set to true for testing purposes
 
 if $KEEP_ORIGINAL_FILES; then
 	REMOVE_SOURCE_FILES=""

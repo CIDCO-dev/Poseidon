0b77fb2be83b39830032a9e07ffeabb43464aa12
Jacobb13 <jacob.bellavance@cidco.ca>
2022-04-21 19:13:49 +0000

    Publishing on topic is functioning

--- src/workspace/src/water_pump/scripts/bilge_pump.py
+++ src/workspace/src/water_pump/scripts/bilge_pump.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
 import rospy
-from bilge_pump_msg.msg import WaterIntrusion
 from std_msgs.msg import String
+from water_pump.msg import WaterIntrusion
+from water_pump.msg import PumpControl
 import serial
+
+serialPort = serial.Serial("/dev/ttyACM0", 9200, timeout=1)
+
+def pump_callBack(data):
+	if data.ActivatePump == 1:
+		serialPort.write(b'gpio set 1\r')
+	else :
+		serialPort.write(b'gpio clear 1\r') 
 
 def waterAlertInspector():
 	rospy.init_node('bilgePump',anonymous="true")
 	pub = rospy.Publisher('waterInspection', WaterIntrusion, queue_size=5)
+	rospy.Subscriber('pump_control', PumpControl, pump_callBack)
 	rate = rospy.Rate(10)
-	serialPort = serial.Serial("/dev/ttyACM0", 9200, timeout=1)
+	#serialPort = serial.Serial("/dev/ttyACM0", 9200, timeout=1)
+	#rospy.spin()
+
 	while not rospy.is_shutdown():
 		#send "gpio read" command
 		serialPort.write(b'gpio read 0\r')
@@ -18,13 +30,18 @@ def waterAlertInspector():
 		msg = WaterIntrusion()
 		if serialread[13] == 49:
 			rospy.loginfo("ALERT : Water intrusion !\r")
-			WaterIntrusion.status = 1
+			msg.status = 1
+			#pub.publish(msg)
 		else :
 			# No water intrusion !
-			WaterIntrusion.status = 0
-		#WaterIntrusion.header = rospy.get_time()
-		pub.publish()
+			msg.status = 0
+			#pub.publish(msg)
+		msg.header.stamp = rospy.get_rostime()
+		pub.publish(msg)
 		rate.sleep()
+
+
+
 
 if __name__ == "__main__":
 	try:

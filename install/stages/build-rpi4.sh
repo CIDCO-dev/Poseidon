d56e17415fffe13096da899dbc6b9e1146dae5b1
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2023-01-09 12:56:33 -0500

    disable video recorder in the rpi build

--- install/stages/build-rpi4.sh
+++ install/stages/build-rpi4.sh
@@ -6,7 +6,7 @@ echo "[+] Building Poseidon"
 
 cd /home/ubuntu/Poseidon/src/workspace
 source /opt/ros/noetic/setup.bash
-catkin_make -j1 -DCATKIN_BLACKLIST_PACKAGES="mavros;libmavconn;mavros_extras;echoboat_odometry;mavros_msgs;test_mavros"
+catkin_make -j1 -DCATKIN_BLACKLIST_PACKAGES="mavros;libmavconn;mavros_extras;echoboat_odometry;mavros_msgs;test_mavros;video_recorder"
 
 [ ! -d "/home/ubuntu/Poseidon/www/webroot/record" ] && mkdir /home/ubuntu/Poseidon/www/webroot/record
 

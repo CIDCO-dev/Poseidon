941a5b2e3a2167b95b8d9145653d0a9238893f87
patrick charron morneau <patcmorneau@gmail.com>
2023-04-14 16:16:00 -0400

    compile fix

--- src/workspace/src/logger/src/loggerBase.cpp
+++ src/workspace/src/logger/src/loggerBase.cpp
@@ -19,12 +19,11 @@ LoggerBase::LoggerBase(std::string & outputFolder):outputFolder(outputFolder), t
 	
 	configurationClient = node.serviceClient<setting_msg::ConfigurationService>("get_configuration");
 	
-	if (!node->getParam("fileExtensionForGpsDatagram", this->fileExtensionForGpsDatagram))
+	if (!node.getParam("fileExtensionForGpsDatagram", this->fileExtensionForGpsDatagram))
 	{
 		ROS_ERROR_STREAM("No protocol file extention defined, defaulting to .bin");
 		this->fileExtensionForGpsDatagram = ".bin";
 	}
-	ROS_ERROR_STREAM()
 	
 	updateLogRotationInterval();
 	updateTranferConfig();

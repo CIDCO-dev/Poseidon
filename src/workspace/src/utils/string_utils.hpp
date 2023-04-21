6dd945e1a53a351de701f71b8d17dce85901ef2e
patrick charron morneau <patcmorneau@gmail.com>
2023-04-21 11:44:16 -0400

    wifi config node

---
+++ src/workspace/src/utils/string_utils.hpp
@@ -0,0 +1,27 @@
+#ifndef string_utils
+#define string_utils
+
+#include <iostream>
+#include <algorithm>
+
+std::string & ltrim(std::string & str)
+{
+  auto it2 =  std::find_if( str.begin() , str.end() , [](char ch){ return !std::isspace<char>(ch , std::locale::classic() ) ; } );
+  str.erase( str.begin() , it2);
+  return str;
+}
+
+std::string & rtrim(std::string & str)
+{
+  auto it1 =  std::find_if( str.rbegin() , str.rend() , [](char ch){ return !std::isspace<char>(ch , std::locale::classic() ) ; } );
+  str.erase( it1.base() , str.end() );
+  return str;
+}
+
+
+std::string trimSpaces(std::string & str){
+	
+	return ltrim(rtrim(str));
+}
+
+#endif

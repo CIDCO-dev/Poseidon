0e922f1045a076b5da9cb1d51b3441bb00d0abf6
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2023-10-18 16:34:47 -0400

    add launchROSService.sh in helper

--- helper_script/restore_after_update.sh
+++ helper_script/restore_after_update.sh
@@ -6,6 +6,7 @@ systemctl stop ros
 cp /opt/Poseidon.backup/calibration.dat /opt/Poseidon
 cp /opt/Poseidon.backup/config.txt /opt/Poseidon
 cp /opt/Poseidon.backup/uart_on_boot.sh /opt/Poseidon/service
+cp /opt/Poseidon.backup/launchROSSServoce.sh /opt/Poseidon
 
 now=`date +"%Y-%m-%d"`
 

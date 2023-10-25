8f8b1d77394da731c778250e7a92232b48ec4292
Dany Doiron <dany.doiron@cidco.ca>
2023-10-25 20:23:08 +0000

    adding ros noetic installation to the script and adding *.log in the gitignore file

--- install/setup.py
+++ install/setup.py
@@ -9,7 +9,22 @@ import inquirer
 import subprocess
 
 
-def loadvariable()
+system = "notset"
+isubuntu = False
+version = "notset"
+processor = "notset"
+ram = "notset"
+swap = "notset"
+
+
+def loadvariable():
+    global system
+    global isubuntu
+    global version
+    global processor
+    global ram
+    global swap
+
     def check_ubuntu_distribution():
         if platform.system() == 'Linux':
             try:
@@ -84,33 +99,88 @@ def check_install():
     else:
         print('[!] New installation.')
 
-
-def install_ros_melodic();
-        try:
-            subprocess.run(["apt-get", "update"], check=True)
-            print(f" a été installé avec succès.")
-        except subprocess.CalledProcessError:
-            print(f"Erreur lors de l'installation de .")
-        try:
-            subprocess.run(["apt-get", "upgrade"], check=True)
-            print(f" a été installé avec succès.")
-        except subprocess.CalledProcessError:
-            print(f"Erreur lors de l'installation de .")
+def install_package(package_name):
+    try:
+        log_file = open("install.log", "a") 
+        subprocess.run(["sudo", "apt-get", "install", package_name], check=True, stdout=log_file, stderr=log_file)
+        log_file.close()
+        print(f"[!] {package_name} have been installed successfully.")
+    except subprocess.CalledProcessError:
+        print(f"[X] Error during the installation of {package_name}.")
 
 
-def install_ros_noetic();
-        try:
-            subprocess.run(["apt-get", "update"], check=True)
-            print(f" a été installé avec succès.")
-        except subprocess.CalledProcessError:
-            print(f"Erreur lors de l'installation de .")
-        try:
-            subprocess.run(["apt-get", "upgrade"], check=True)
-            print(f" a été installé avec succès.")
-        except subprocess.CalledProcessError:
-            print(f"Erreur lors de l'installation de .")
+
+def update_packager():
+    try:
+        log_file = open("install.log", "a") 
+        subprocess.run(["apt-get", "update"], check=True, stdout=log_file, stderr=log_file)
+        log_file.close()
+        print(f"[!] Successful update of the package list.")
+    except subprocess.CalledProcessError: 
+        print(f"[X] Error during the update of the package list.")
+    try:
+        log_file = open("install.log", "a")
+        subprocess.run(["apt-get", "upgrade", "-y"], check=True, stdout=log_file, stderr=log_file)
+        log_file.close()
+        print(f"[!] Success for the package update.")
+    except subprocess.CalledProcessError:
+        print(f"[X] Error during the package update .")
+
+def install_ros_melodic():
+    exit()
+
+def install_ros_noetic():
+    try:
+        log_file = open("install.log", "a") 
+        command = ['/bin/sh', '-c', 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list']
+        subprocess.run(command, check=True, stdout=log_file, stderr=log_file)
+        log_file.close()
+        print(f"[!] Successful append ROS sources.")
+    except subprocess.CalledProcessError: 
+        print(f"[X] Error during the ROS sources append.")
+    try:
+        log_file = open("install.log", "a") 
+        curl_command = ['/usr/bin/curl', '-s', 'https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc']
+        apt_key_command = ['/usr/bin/apt-key', 'add', '-']
+        curl_process = subprocess.Popen(curl_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        apt_key_process = subprocess.Popen(apt_key_command, stdin=curl_process.stdout, stdout=log_file, stderr=log_file)
+        curl_process.stdout.close()
+        apt_key_process.communicate()
+        log_file.close()        
+        print(f"[!] The key have been installed.")
+    except subprocess.CalledProcessError: 
+        print(f"[X] Error during key installation.")
+    update_packager()
+    install_package("ros-noetic-ros-base")
+    install_package("ros-noetic-tf2-geometry-msgs")
+    install_package("g++")
+    install_package("python3-rosdep")
+    install_package("ros-noetic-mavros")
+    install_package("ros-noetic-sbg-driver")
+    install_package("python3-rosinstall")
+    install_package("python3-rosinstall-generator")
+    install_package("python3-wstool")
+    install_package("build-essential")
+    #rosdep init
+    #gpsd client
+
+def install_toolchain():
+    update_packager()
+    install_package("gcc")
+    install_package("python3-dev")
+    install_package("python3-pip")
+    install_package("python-setuptools")
+    install_package("git")
+    install_package("curl")
+    install_package("zip")
 
 def install():
+    global system
+    global isubuntu
+    global version
+    global processor
+    global ram
+    global swap
     if system != "Linux" and isubuntu:
         return print("[!] Please use Ubuntu as OS!")
     elif os.geteuid() != 0:
@@ -119,16 +189,14 @@ def install():
         return print("Not enough RAM and SWAP.")
         # implement swap gestion
 
-    if version == 18 and processor == "x86_64":
-        os.system('/opt/Poseidon/install/x64-melodic.sh')
-    elif version == 18 and processor.lower().startswith("armv"):
-        os.system('/opt/Poseidon/install/rpi4-noetic.sh')
+    install_toolchain()
+    if version == 18:
+        install_ros_melodic()
 
-    if version == 20 and processor == "x86_64":
-        os.system('/opt/Poseidon/install/x64-noetic.sh')
-    elif version == 20 and processor.lower().startswith("armv"):
-        os.system('/opt/Poseidon/install/rpi4-noetic.sh')        
+
+    if version == 20:
+        install_ros_noetic()
 
 loadvariable()
 check_install()
-#install()
+install()

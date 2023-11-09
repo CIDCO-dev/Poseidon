a1b27d8be3e6a91877b959c16515d9043621fc93
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2023-11-09 14:13:05 -0500

    add helper for os update copy sonar imagenex in a directory with C program

---
+++ src/workspace/src/sonar_imagenex852_c/tests/test_sonar_imagenex852_node.cpp
@@ -0,0 +1,48 @@
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+
+#include <thread>
+#include <unistd.h>
+
+#include "geometry_msgs/PointStamped.h"
+
+//global variable to test if callback was called by subscriber
+bool subscriberReceivedData = false;
+void callback_AssertSubscriberReceivedData(const geometry_msgs::PointStamped & sonarMsg)
+{
+    if(!subscriberReceivedData) {
+        subscriberReceivedData = true;
+    }
+
+    return;
+}
+
+TEST(ImuDummyTestSuite, testCaseSubscriberReceivedWhatIsPublished) {
+
+    ros::NodeHandle nh;
+
+    //setup subscriber with callback that will assert if test passes
+    ros::Subscriber sub = nh.subscribe("depth", 1000, callback_AssertSubscriberReceivedData);
+
+    // wait for subscriber to receive data from imu
+    sleep(2);
+
+    //verify that callback was called by subscriber
+    ASSERT_TRUE(subscriberReceivedData) << "callback was not called by subscriber";
+}
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "TestImagenex852Node");
+
+    testing::InitGoogleTest(&argc, argv);
+
+    std::thread t([]{ros::spin();}); // let ros spin in its own thread
+
+    auto res = RUN_ALL_TESTS();
+
+    ros::shutdown(); // this will cause the ros::spin() to return
+    t.join();
+
+    return res;
+}

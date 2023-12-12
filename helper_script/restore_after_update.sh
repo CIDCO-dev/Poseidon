4452a27835536d14e6ce5f226693c81a8f930479
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2023-12-12 10:13:29 -0500

    fix poseidon backup script and backup filename

--- helper_script/restore_after_update.sh
+++ helper_script/restore_after_update.sh
@@ -8,8 +8,6 @@ cp /opt/Poseidon.backup/config.txt /opt/Poseidon
 cp /opt/Poseidon.backup/uart_on_boot.sh /opt/Poseidon/service
 cp /opt/Poseidon.backup/launchROSSServoce.sh /opt/Poseidon
 
-now=`date +"%Y-%m-%d"`
 
-zip -r /opt/Poseidon/www/webroot/record/Poseidon.config.${now}.zip /opt/Poseidon.backup
 
 systemctl start ros

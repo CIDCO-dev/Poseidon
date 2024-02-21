fdc72c5fbd068a32107f0b727d41f5fea0e360ce
patrick <patcmorneau@cidco.ca>
2024-02-21 15:22:49 -0500

    serial baud rate

--- src/workspace/src/hydroball_config_websocket/include/hydroball_config_websocket/hydroball_config_websocket.h
+++ src/workspace/src/hydroball_config_websocket/include/hydroball_config_websocket/hydroball_config_websocket.h
@@ -247,9 +247,7 @@ public:
 			setting.key   =i->first;
 			setting.value =i->second;
 			configTopic.publish(setting);
-			std::cout<<setting.key << " , " << setting.value <<"\n";
 		}
-		std::cout<<"end of broadcast config \n";
 	}
 
 	void broadcastImuTransform(){

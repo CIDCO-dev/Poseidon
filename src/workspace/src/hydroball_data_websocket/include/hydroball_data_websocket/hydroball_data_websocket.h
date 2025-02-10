d2e679f10d75bee3ea24d92aef5e3144be655241
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2025-02-10 16:25:37 -0500

    add new dataset in websocket

--- src/workspace/src/hydroball_data_websocket/include/hydroball_data_websocket/hydroball_data_websocket.h
+++ src/workspace/src/hydroball_data_websocket/include/hydroball_data_websocket/hydroball_data_websocket.h
@@ -254,6 +254,11 @@ public:
 			rapidjson::Value temp((int) state.vitals.temperature);
 			rapidjson::Value voltage(state.vitals.voltage);
 			rapidjson::Value humidity((int) state.vitals.humidity);
+			rapidjson::Value IsErrorOn( state.vitals.IsErrorOn);
+			rapidjson::Value isWarningOn( state.vitals.isWarningOn);
+			rapidjson::Value isNoFixOn(state.vitals.isNoFixOn);
+			rapidjson::Value status;
+			status.SetString(state.vitals.status.c_str(), telemetry.GetAllocator());
 
 			vitalsArray.PushBack(cputemp, telemetry.GetAllocator()); //0
 			vitalsArray.PushBack(cpuload, telemetry.GetAllocator());
@@ -263,7 +268,12 @@ public:
 			vitalsArray.PushBack(temp, telemetry.GetAllocator()); //5
 			vitalsArray.PushBack(voltage, telemetry.GetAllocator());//6
 			vitalsArray.PushBack(humidity, telemetry.GetAllocator());//7
+			vitalsArray.PushBack(IsErrorOn, telemetry.GetAllocator());
+			vitalsArray.PushBack(isWarningOn, telemetry.GetAllocator());
+			vitalsArray.PushBack(isNoFixOn, telemetry.GetAllocator());
+
 			telemetry.AddMember("vitals", vitalsArray, telemetry.GetAllocator());
+			telemetry.AddMember("status", status, telemetry.GetAllocator());
 		}
 
 		document.AddMember("telemetry", telemetry, document.GetAllocator());

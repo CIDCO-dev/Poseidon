b3aea49096ebd996d90d6c1d05b8583ebb9b2d84
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2025-02-11 10:24:57 -0500

    code cleaning

--- www/webroot/js/mapScript.js
+++ www/webroot/js/mapScript.js
@@ -29,8 +29,7 @@ var oldstate = 0;
 navMap = new L.map("navMap").setView([0, 0], 23);
 var polycolor = "";
 
-//Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-//Chart.defaults.global.defaultFontColor = '#858796'
+
 
 L.control.scale().addTo(navMap);
 var legend = L.control({position: 'bottomright'});
@@ -87,53 +86,6 @@ var panControl = L.control({position: 'topright'});
 var currentZoom = navMap.getZoom();
 
 
-
-//	navMap = new L.map("navMap").setView([0, 0], 1);	
-//        var shpfile = new L.Shapefile("map/TM_WORLD_BORDERS-0.3.zip", {
-//                onEachFeature: function(feature, layer) {
-//                        if (feature.properties) {
-//                                layer.bindPopup(Object.keys(feature.properties).map(function(k) {
-//                                        return k + ": " + feature.properties[k];
-//                                }).join("<br />"), {
-//                                        maxHeight: 200
-//                                });
-//                        }
-//                }
-//        });
-
-//        shpfile.addTo(navMap);
-//	navMap.invalidateSize();
-
-/*
-function processState(msg) {
-  if (msg.telemetry.vitals[4] > oldstate) {
-    oldstate = msg.telemetry.vitals[4];
-    currentZoom = navMap.getZoom();
-    curentweight = (23 - currentZoom) * 6 + 23;
-    //positionne la carte sur la position gps
-    navMap.panTo([msg.telemetry.position[1], msg.telemetry.position[0]]);
-
-    //dessine la ligne parcourue
-    latlon = [msg.telemetry.position[1], msg.telemetry.position[0]];
-    qp = [msg.telemetry.position[1], msg.telemetry.position[0], msg.telemetry.depth[0]];
-    currentpolyline.push(latlon);
-    quakePoints.push(qp);
-    polyline.remove();
-    hotlineLayer.remove();
-    polyline = L.polyline([currentpolyline], { weight: 2, opacity: 1, smoothFactor: smootf, color: 'red' }).addTo(navMap);
-    hotlineLayer = L.hotline(quakePoints, { min: 7, max: 10, palette: { 0.0: '#ff0000', 0.2: '#ffff00', 0.4: '#00ff00', 0.6: '#00ffff', 0.8: '#0000ff', 0.9: '#8000ff', 1.0: '#000000' }, smoothFactor: smootf, weight: curentweight, outlineColor: '#000000', outlineWidth: 0 }).addTo(navMap);
-    polyLatLngs = polyline.getLatLngs();
-
-    //if (currentpolyline.length == 200){
-    //        polyline1 = L.polyline(currentpolyline,{weight: 2,opacity:1, smoothFactor: smootf , color:'red'}).addTo(m);
-    //        currentpolyline= [latlon];
-    //        hotlineLayer1 =  L.hotline(quakePoints, {min: 7,max: 10,palette: {0.0: '#ff0000',0.2: '#ffff00',0.4: '#00ff00',0.6: '#00ffff',0.8: '#0000ff',0.9: '#8000ff', 1.0: '#000000'},smoothFactor: smootf ,weight: 20,outlineColor: '#000000', outlineWidth: 0}).addTo(m);
-    //        quakePoints = [qp];
-    //        }
-
-  }
-}
-*/
 function processState(msg) {
   // Assurez-vous que msg et msg.telemetry existent et contiennent les propriétés attendues
   if (msg && msg.telemetry && msg.telemetry.vitals && msg.telemetry.position && msg.telemetry.depth) {

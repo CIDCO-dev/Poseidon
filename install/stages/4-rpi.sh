3b7624706db4dbde6944756adca641b948ffdd87
Dany Doiron <58708562+Ddoiron-cidco@users.noreply.github.com>
2025-04-10 10:16:30 -0400

    fix i2c4 configuration

--- install/stages/4-rpi.sh
+++ install/stages/4-rpi.sh
@@ -40,7 +40,7 @@ sudo bash -c 'echo "pps-ldisc" >> /etc/modules'
 
 echo "[+] Config I2C"
 sudo bash -c 'echo "dtparam=i2c_arm_baudrate=500000" >> /boot/firmware/usercfg.txt'
-sudo bash -c 'echo "dtparam=i2c4,pins_6_7" >> /boot/firmware/usercfg.txt'
+sudo bash -c 'echo "dtoverlay=i2c4,pins_6_7" >> /boot/firmware/usercfg.txt'
 
 echo "[+] Turn off SPI"
 sudo sed -i "s/dtparam=spi=on/dtparam=spi=off/g" /boot/firmware/syscfg.txt
